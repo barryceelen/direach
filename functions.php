31ed37ffcef867eb3e985c970e210eba9b9ffa73
Thomas Bibby <thomas@bibby.ie>
2016-07-31 11:46:22 +0100

    Enqueuing normalise stylesheet v2

--- functions.php
+++ functions.php
@@ -103,7 +103,7 @@ add_action( 'widgets_init', 'direach_widgets_init' );
  */
 function direach_scripts() {
 // load the normalise stylesheet first
-	wp_enqueue_style( 'normalise', get_template_directory_uri() . 'normalise.css');
+	wp_enqueue_style( 'normalise', get_template_directory_uri() . '/normalise.css');
 	wp_enqueue_style( 'direach-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'direach-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
