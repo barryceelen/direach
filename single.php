5216713957b0815691853b62e941c2ebea7e91f4
Thomas Bibby <thomas@bibby.ie>
2016-08-08 15:45:13 +0100

    post navigation

--- single.php
+++ single.php
@@ -17,9 +17,9 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+			
 			get_template_part( 'inc/direach', 'masthead');
-
+			the_post_navigation();
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
