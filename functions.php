62586c4746d04636e022b8c03241949f3891d39d
Thomas Bibby <thomas@bibby.ie>
2016-08-08 14:50:32 +0100

    Removing js calls

--- functions.php
+++ functions.php
@@ -105,10 +105,10 @@ function direach_scripts() {
 // load the normalise stylesheet first
 	wp_enqueue_style( 'normalise', get_template_directory_uri() . '/normalise.css');
 	wp_enqueue_style( 'direach-style', get_stylesheet_uri() );
+//remove javascript
+	//wp_enqueue_script( 'direach-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'direach-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'direach-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	//wp_enqueue_script( 'direach-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
