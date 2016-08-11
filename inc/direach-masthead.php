828e86171637a018449104abc96bf98588f23c50
Thomas Bibby <thomas@bibby.ie>
2016-08-11 17:34:11 +0100

    testing get_header_image_tag

--- inc/direach-masthead.php
+++ inc/direach-masthead.php
@@ -18,7 +18,6 @@
 			if ( get_header_image() ) : ?>
 	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 		<?php echo get_header_image_tag(); ?>
-		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
 	</a>
 	<?php endif; // End header image check. 
 			
